a950521ecbbe0a71625b7a8f4038b7689be9f916
mantuoluozk <1741695059@qq.com>
2018-12-20 20:41:15 +0800

    Create ui_subwindow.h

---
+++ ui_subwindow.h
@@ -0,0 +1,472 @@
+/********************************************************************************
+** Form generated from reading UI file 'subwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.5.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_SUBWINDOW_H
+#define UI_SUBWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QCheckBox>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QScrollArea>
+#include <QtWidgets/QStackedWidget>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_SubWindow
+{
+public:
+    QStackedWidget *stackedWidget;
+    QWidget *page_7;
+    QGridLayout *gridLayout_3;
+    QCheckBox *button00;
+    QCheckBox *button01;
+    QCheckBox *button02;
+    QCheckBox *button03;
+    QCheckBox *button04;
+    QCheckBox *button05;
+    QWidget *page;
+    QHBoxLayout *horizontalLayout;
+    QCheckBox *button10;
+    QCheckBox *button11;
+    QCheckBox *button12;
+    QWidget *page_2;
+    QHBoxLayout *horizontalLayout_2;
+    QCheckBox *button20;
+    QCheckBox *button21;
+    QCheckBox *button22;
+    QWidget *page_3;
+    QHBoxLayout *horizontalLayout_4;
+    QCheckBox *checkBox_7;
+    QCheckBox *checkBox_6;
+    QCheckBox *checkBox_8;
+    QWidget *page_4;
+    QHBoxLayout *horizontalLayout_3;
+    QCheckBox *checkBox;
+    QCheckBox *checkBox_9;
+    QCheckBox *checkBox_10;
+    QCheckBox *checkBox_2;
+    QWidget *page_5;
+    QGridLayout *gridLayout;
+    QCheckBox *checkBox_20;
+    QCheckBox *checkBox_21;
+    QCheckBox *checkBox_22;
+    QCheckBox *checkBox_23;
+    QWidget *page_6;
+    QGridLayout *gridLayout_2;
+    QCheckBox *checkBox_24;
+    QCheckBox *checkBox_25;
+    QCheckBox *checkBox_27;
+    QCheckBox *checkBox_26;
+    QScrollArea *scrollArea;
+    QWidget *scrollAreaWidgetContents;
+    QVBoxLayout *verticalLayout;
+    QPushButton *button0;
+    QPushButton *button1;
+    QPushButton *button2;
+    QPushButton *button3;
+    QPushButton *button4;
+    QPushButton *button5;
+    QPushButton *button6;
+    QPushButton *pushButton_11;
+    QPushButton *pushButton_14;
+    QPushButton *pushButton_15;
+    QPushButton *pushButton_16;
+    QPushButton *pushButton_12;
+    QPushButton *pushButton_33;
+    QPushButton *pushButton_34;
+    QPushButton *pushButton_35;
+    QPushButton *pushButton_36;
+    QPushButton *pushButton_37;
+    QPushButton *pushButton_38;
+    QPushButton *pushButton_39;
+    QPushButton *pushButton_40;
+    QPushButton *pushButton_41;
+    QPushButton *pushButton_42;
+
+    void setupUi(QDialog *SubWindow)
+    {
+        if (SubWindow->objectName().isEmpty())
+            SubWindow->setObjectName(QStringLiteral("SubWindow"));
+        SubWindow->resize(737, 531);
+        stackedWidget = new QStackedWidget(SubWindow);
+        stackedWidget->setObjectName(QStringLiteral("stackedWidget"));
+        stackedWidget->setEnabled(true);
+        stackedWidget->setGeometry(QRect(190, 50, 500, 451));
+        stackedWidget->setMinimumSize(QSize(500, 451));
+        stackedWidget->setMaximumSize(QSize(741, 451));
+        page_7 = new QWidget();
+        page_7->setObjectName(QStringLiteral("page_7"));
+        gridLayout_3 = new QGridLayout(page_7);
+        gridLayout_3->setObjectName(QStringLiteral("gridLayout_3"));
+        button00 = new QCheckBox(page_7);
+        button00->setObjectName(QStringLiteral("button00"));
+
+        gridLayout_3->addWidget(button00, 0, 0, 1, 1);
+
+        button01 = new QCheckBox(page_7);
+        button01->setObjectName(QStringLiteral("button01"));
+
+        gridLayout_3->addWidget(button01, 0, 1, 1, 1);
+
+        button02 = new QCheckBox(page_7);
+        button02->setObjectName(QStringLiteral("button02"));
+
+        gridLayout_3->addWidget(button02, 0, 2, 1, 1);
+
+        button03 = new QCheckBox(page_7);
+        button03->setObjectName(QStringLiteral("button03"));
+
+        gridLayout_3->addWidget(button03, 0, 3, 1, 1);
+
+        button04 = new QCheckBox(page_7);
+        button04->setObjectName(QStringLiteral("button04"));
+
+        gridLayout_3->addWidget(button04, 0, 4, 1, 1);
+
+        button05 = new QCheckBox(page_7);
+        button05->setObjectName(QStringLiteral("button05"));
+
+        gridLayout_3->addWidget(button05, 0, 5, 1, 1);
+
+        stackedWidget->addWidget(page_7);
+        page = new QWidget();
+        page->setObjectName(QStringLiteral("page"));
+        horizontalLayout = new QHBoxLayout(page);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        button10 = new QCheckBox(page);
+        button10->setObjectName(QStringLiteral("button10"));
+
+        horizontalLayout->addWidget(button10);
+
+        button11 = new QCheckBox(page);
+        button11->setObjectName(QStringLiteral("button11"));
+
+        horizontalLayout->addWidget(button11);
+
+        button12 = new QCheckBox(page);
+        button12->setObjectName(QStringLiteral("button12"));
+
+        horizontalLayout->addWidget(button12);
+
+        stackedWidget->addWidget(page);
+        page_2 = new QWidget();
+        page_2->setObjectName(QStringLiteral("page_2"));
+        horizontalLayout_2 = new QHBoxLayout(page_2);
+        horizontalLayout_2->setObjectName(QStringLiteral("horizontalLayout_2"));
+        button20 = new QCheckBox(page_2);
+        button20->setObjectName(QStringLiteral("button20"));
+
+        horizontalLayout_2->addWidget(button20);
+
+        button21 = new QCheckBox(page_2);
+        button21->setObjectName(QStringLiteral("button21"));
+
+        horizontalLayout_2->addWidget(button21);
+
+        button22 = new QCheckBox(page_2);
+        button22->setObjectName(QStringLiteral("button22"));
+
+        horizontalLayout_2->addWidget(button22);
+
+        stackedWidget->addWidget(page_2);
+        page_3 = new QWidget();
+        page_3->setObjectName(QStringLiteral("page_3"));
+        horizontalLayout_4 = new QHBoxLayout(page_3);
+        horizontalLayout_4->setObjectName(QStringLiteral("horizontalLayout_4"));
+        checkBox_7 = new QCheckBox(page_3);
+        checkBox_7->setObjectName(QStringLiteral("checkBox_7"));
+
+        horizontalLayout_4->addWidget(checkBox_7);
+
+        checkBox_6 = new QCheckBox(page_3);
+        checkBox_6->setObjectName(QStringLiteral("checkBox_6"));
+
+        horizontalLayout_4->addWidget(checkBox_6);
+
+        checkBox_8 = new QCheckBox(page_3);
+        checkBox_8->setObjectName(QStringLiteral("checkBox_8"));
+
+        horizontalLayout_4->addWidget(checkBox_8);
+
+        stackedWidget->addWidget(page_3);
+        page_4 = new QWidget();
+        page_4->setObjectName(QStringLiteral("page_4"));
+        horizontalLayout_3 = new QHBoxLayout(page_4);
+        horizontalLayout_3->setObjectName(QStringLiteral("horizontalLayout_3"));
+        checkBox = new QCheckBox(page_4);
+        checkBox->setObjectName(QStringLiteral("checkBox"));
+
+        horizontalLayout_3->addWidget(checkBox);
+
+        checkBox_9 = new QCheckBox(page_4);
+        checkBox_9->setObjectName(QStringLiteral("checkBox_9"));
+
+        horizontalLayout_3->addWidget(checkBox_9);
+
+        checkBox_10 = new QCheckBox(page_4);
+        checkBox_10->setObjectName(QStringLiteral("checkBox_10"));
+
+        horizontalLayout_3->addWidget(checkBox_10);
+
+        checkBox_2 = new QCheckBox(page_4);
+        checkBox_2->setObjectName(QStringLiteral("checkBox_2"));
+
+        horizontalLayout_3->addWidget(checkBox_2);
+
+        stackedWidget->addWidget(page_4);
+        page_5 = new QWidget();
+        page_5->setObjectName(QStringLiteral("page_5"));
+        gridLayout = new QGridLayout(page_5);
+        gridLayout->setObjectName(QStringLiteral("gridLayout"));
+        checkBox_20 = new QCheckBox(page_5);
+        checkBox_20->setObjectName(QStringLiteral("checkBox_20"));
+
+        gridLayout->addWidget(checkBox_20, 0, 0, 1, 1);
+
+        checkBox_21 = new QCheckBox(page_5);
+        checkBox_21->setObjectName(QStringLiteral("checkBox_21"));
+
+        gridLayout->addWidget(checkBox_21, 0, 1, 1, 1);
+
+        checkBox_22 = new QCheckBox(page_5);
+        checkBox_22->setObjectName(QStringLiteral("checkBox_22"));
+
+        gridLayout->addWidget(checkBox_22, 0, 2, 1, 1);
+
+        checkBox_23 = new QCheckBox(page_5);
+        checkBox_23->setObjectName(QStringLiteral("checkBox_23"));
+
+        gridLayout->addWidget(checkBox_23, 0, 3, 1, 1);
+
+        stackedWidget->addWidget(page_5);
+        page_6 = new QWidget();
+        page_6->setObjectName(QStringLiteral("page_6"));
+        gridLayout_2 = new QGridLayout(page_6);
+        gridLayout_2->setObjectName(QStringLiteral("gridLayout_2"));
+        checkBox_24 = new QCheckBox(page_6);
+        checkBox_24->setObjectName(QStringLiteral("checkBox_24"));
+
+        gridLayout_2->addWidget(checkBox_24, 0, 0, 1, 1);
+
+        checkBox_25 = new QCheckBox(page_6);
+        checkBox_25->setObjectName(QStringLiteral("checkBox_25"));
+
+        gridLayout_2->addWidget(checkBox_25, 0, 1, 1, 1);
+
+        checkBox_27 = new QCheckBox(page_6);
+        checkBox_27->setObjectName(QStringLiteral("checkBox_27"));
+
+        gridLayout_2->addWidget(checkBox_27, 0, 2, 1, 1);
+
+        checkBox_26 = new QCheckBox(page_6);
+        checkBox_26->setObjectName(QStringLiteral("checkBox_26"));
+
+        gridLayout_2->addWidget(checkBox_26, 0, 3, 1, 1);
+
+        stackedWidget->addWidget(page_6);
+        scrollArea = new QScrollArea(SubWindow);
+        scrollArea->setObjectName(QStringLiteral("scrollArea"));
+        scrollArea->setGeometry(QRect(0, 0, 161, 531));
+        scrollArea->setWidgetResizable(true);
+        scrollAreaWidgetContents = new QWidget();
+        scrollAreaWidgetContents->setObjectName(QStringLiteral("scrollAreaWidgetContents"));
+        scrollAreaWidgetContents->setGeometry(QRect(0, 0, 142, 650));
+        verticalLayout = new QVBoxLayout(scrollAreaWidgetContents);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        button0 = new QPushButton(scrollAreaWidgetContents);
+        button0->setObjectName(QStringLiteral("button0"));
+
+        verticalLayout->addWidget(button0);
+
+        button1 = new QPushButton(scrollAreaWidgetContents);
+        button1->setObjectName(QStringLiteral("button1"));
+
+        verticalLayout->addWidget(button1);
+
+        button2 = new QPushButton(scrollAreaWidgetContents);
+        button2->setObjectName(QStringLiteral("button2"));
+
+        verticalLayout->addWidget(button2);
+
+        button3 = new QPushButton(scrollAreaWidgetContents);
+        button3->setObjectName(QStringLiteral("button3"));
+
+        verticalLayout->addWidget(button3);
+
+        button4 = new QPushButton(scrollAreaWidgetContents);
+        button4->setObjectName(QStringLiteral("button4"));
+
+        verticalLayout->addWidget(button4);
+
+        button5 = new QPushButton(scrollAreaWidgetContents);
+        button5->setObjectName(QStringLiteral("button5"));
+
+        verticalLayout->addWidget(button5);
+
+        button6 = new QPushButton(scrollAreaWidgetContents);
+        button6->setObjectName(QStringLiteral("button6"));
+
+        verticalLayout->addWidget(button6);
+
+        pushButton_11 = new QPushButton(scrollAreaWidgetContents);
+        pushButton_11->setObjectName(QStringLiteral("pushButton_11"));
+
+        verticalLayout->addWidget(pushButton_11);
+
+        pushButton_14 = new QPushButton(scrollAreaWidgetContents);
+        pushButton_14->setObjectName(QStringLiteral("pushButton_14"));
+
+        verticalLayout->addWidget(pushButton_14);
+
+        pushButton_15 = new QPushButton(scrollAreaWidgetContents);
+        pushButton_15->setObjectName(QStringLiteral("pushButton_15"));
+
+        verticalLayout->addWidget(pushButton_15);
+
+        pushButton_16 = new QPushButton(scrollAreaWidgetContents);
+        pushButton_16->setObjectName(QStringLiteral("pushButton_16"));
+
+        verticalLayout->addWidget(pushButton_16);
+
+        pushButton_12 = new QPushButton(scrollAreaWidgetContents);
+        pushButton_12->setObjectName(QStringLiteral("pushButton_12"));
+
+        verticalLayout->addWidget(pushButton_12);
+
+        pushButton_33 = new QPushButton(scrollAreaWidgetContents);
+        pushButton_33->setObjectName(QStringLiteral("pushButton_33"));
+
+        verticalLayout->addWidget(pushButton_33);
+
+        pushButton_34 = new QPushButton(scrollAreaWidgetContents);
+        pushButton_34->setObjectName(QStringLiteral("pushButton_34"));
+
+        verticalLayout->addWidget(pushButton_34);
+
+        pushButton_35 = new QPushButton(scrollAreaWidgetContents);
+        pushButton_35->setObjectName(QStringLiteral("pushButton_35"));
+
+        verticalLayout->addWidget(pushButton_35);
+
+        pushButton_36 = new QPushButton(scrollAreaWidgetContents);
+        pushButton_36->setObjectName(QStringLiteral("pushButton_36"));
+
+        verticalLayout->addWidget(pushButton_36);
+
+        pushButton_37 = new QPushButton(scrollAreaWidgetContents);
+        pushButton_37->setObjectName(QStringLiteral("pushButton_37"));
+
+        verticalLayout->addWidget(pushButton_37);
+
+        pushButton_38 = new QPushButton(scrollAreaWidgetContents);
+        pushButton_38->setObjectName(QStringLiteral("pushButton_38"));
+
+        verticalLayout->addWidget(pushButton_38);
+
+        pushButton_39 = new QPushButton(scrollAreaWidgetContents);
+        pushButton_39->setObjectName(QStringLiteral("pushButton_39"));
+
+        verticalLayout->addWidget(pushButton_39);
+
+        pushButton_40 = new QPushButton(scrollAreaWidgetContents);
+        pushButton_40->setObjectName(QStringLiteral("pushButton_40"));
+
+        verticalLayout->addWidget(pushButton_40);
+
+        pushButton_41 = new QPushButton(scrollAreaWidgetContents);
+        pushButton_41->setObjectName(QStringLiteral("pushButton_41"));
+
+        verticalLayout->addWidget(pushButton_41);
+
+        pushButton_42 = new QPushButton(scrollAreaWidgetContents);
+        pushButton_42->setObjectName(QStringLiteral("pushButton_42"));
+
+        verticalLayout->addWidget(pushButton_42);
+
+        scrollArea->setWidget(scrollAreaWidgetContents);
+
+        retranslateUi(SubWindow);
+
+        stackedWidget->setCurrentIndex(0);
+
+
+        QMetaObject::connectSlotsByName(SubWindow);
+    } // setupUi
+
+    void retranslateUi(QDialog *SubWindow)
+    {
+        SubWindow->setWindowTitle(QApplication::translate("SubWindow", "Dialog", 0));
+        button00->setText(QApplication::translate("SubWindow", "\351\207\215\345\272\206", 0));
+        button01->setText(QApplication::translate("SubWindow", "\345\214\227\344\272\254", 0));
+        button02->setText(QApplication::translate("SubWindow", "\351\246\231\346\270\257", 0));
+        button03->setText(QApplication::translate("SubWindow", "\344\270\212\346\265\267", 0));
+        button04->setText(QApplication::translate("SubWindow", "\346\276\263\351\227\250", 0));
+        button05->setText(QApplication::translate("SubWindow", "\345\244\251\346\264\245", 0));
+        button10->setText(QApplication::translate("SubWindow", "\345\205\260\345\267\236", 0));
+        button11->setText(QApplication::translate("SubWindow", "\347\231\275\351\223\266", 0));
+        button12->setText(QApplication::translate("SubWindow", "\346\225\246\347\205\214", 0));
+        button20->setText(QApplication::translate("SubWindow", "\347\237\263\345\256\266\345\272\204", 0));
+        button21->setText(QApplication::translate("SubWindow", "\345\224\220\345\261\261", 0));
+        button22->setText(QApplication::translate("SubWindow", "\345\274\240\345\256\266\345\217\243", 0));
+        checkBox_7->setText(QApplication::translate("SubWindow", "\351\203\221\345\267\236", 0));
+        checkBox_6->setText(QApplication::translate("SubWindow", "\345\274\200\345\260\201", 0));
+        checkBox_8->setText(QApplication::translate("SubWindow", "\346\264\233\351\230\263", 0));
+        checkBox->setText(QApplication::translate("SubWindow", "\346\265\216\345\215\227", 0));
+        checkBox_9->setText(QApplication::translate("SubWindow", "\351\235\222\345\262\233", 0));
+        checkBox_10->setText(QApplication::translate("SubWindow", "\347\203\237\345\217\260", 0));
+        checkBox_2->setText(QApplication::translate("SubWindow", "\346\227\245\347\205\247", 0));
+        checkBox_20->setText(QApplication::translate("SubWindow", "\345\215\227\346\230\214", 0));
+        checkBox_21->setText(QApplication::translate("SubWindow", "\344\271\235\346\261\237", 0));
+        checkBox_22->setText(QApplication::translate("SubWindow", "\344\270\212\351\245\266", 0));
+        checkBox_23->setText(QApplication::translate("SubWindow", "\350\265\243\345\267\236", 0));
+        checkBox_24->setText(QApplication::translate("SubWindow", "\346\210\220\351\203\275", 0));
+        checkBox_25->setText(QApplication::translate("SubWindow", "\347\273\265\351\230\263", 0));
+        checkBox_27->setText(QApplication::translate("SubWindow", "\350\207\252\350\264\241", 0));
+        checkBox_26->setText(QApplication::translate("SubWindow", "\346\263\270\345\267\236", 0));
+        button0->setText(QApplication::translate("SubWindow", "\347\203\255\351\227\250", 0));
+        button1->setText(QApplication::translate("SubWindow", "\347\224\230\350\202\203", 0));
+        button2->setText(QApplication::translate("SubWindow", "\346\262\263\345\214\227", 0));
+        button3->setText(QApplication::translate("SubWindow", "\346\262\263\345\215\227", 0));
+        button4->setText(QApplication::translate("SubWindow", "\345\261\261\344\270\234", 0));
+        button5->setText(QApplication::translate("SubWindow", "\346\261\237\350\245\277", 0));
+        button6->setText(QApplication::translate("SubWindow", "\345\233\233\345\267\235", 0));
+        pushButton_11->setText(QApplication::translate("SubWindow", "\346\261\237\350\213\217", 0));
+        pushButton_14->setText(QApplication::translate("SubWindow", "\346\265\231\346\261\237", 0));
+        pushButton_15->setText(QApplication::translate("SubWindow", "\345\256\211\345\276\275", 0));
+        pushButton_16->setText(QApplication::translate("SubWindow", "\351\273\221\351\276\231\346\261\237", 0));
+        pushButton_12->setText(QApplication::translate("SubWindow", "\345\220\211\346\236\227", 0));
+        pushButton_33->setText(QApplication::translate("SubWindow", "\350\276\275\345\256\201", 0));
+        pushButton_34->setText(QApplication::translate("SubWindow", "\345\206\205\350\222\231\345\217\244", 0));
+        pushButton_35->setText(QApplication::translate("SubWindow", "\346\226\260\347\226\206", 0));
+        pushButton_36->setText(QApplication::translate("SubWindow", "\350\245\277\350\227\217", 0));
+        pushButton_37->setText(QApplication::translate("SubWindow", "\351\235\222\346\265\267", 0));
+        pushButton_38->setText(QApplication::translate("SubWindow", "\345\256\201\345\244\217", 0));
+        pushButton_39->setText(QApplication::translate("SubWindow", "\351\231\225\350\245\277", 0));
+        pushButton_40->setText(QApplication::translate("SubWindow", "\345\261\261\350\245\277", 0));
+        pushButton_41->setText(QApplication::translate("SubWindow", "\346\271\226\345\214\227", 0));
+        pushButton_42->setText(QApplication::translate("SubWindow", "\346\271\226\345\215\227", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class SubWindow: public Ui_SubWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_SUBWINDOW_H
