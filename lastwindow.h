4bd1a9841af2557608390ccdb54b1fc2253385ad
mantuoluozk <1741695059@qq.com>
2018-12-20 20:40:37 +0800

    Create lastwindow.h

---
+++ lastwindow.h
@@ -0,0 +1,62 @@
+#ifndef LASTWINDOW_H
+#define LASTWINDOW_H
+
+#include <QDialog>
+
+#include <QMainWindow>
+#include <QPushButton>
+#include <QPainter>
+#include <QLabel>
+namespace Ui {
+class LastWindow;
+}
+
+class LastWindow : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit LastWindow(QWidget *parent = 0);
+    ~LastWindow();
+    void sendSlot();
+    void addName(QString);
+    void sendSlotclear();
+
+    void addlabel_cq();//市
+    void addlabel_bj();
+    void addlabel_xg();
+    void addlabel_sh();
+    void addlabel_tj();
+    void addlabel_am();
+    void addlabel_lz();
+    void addlabel_by();
+    void addlabel_dh();
+    void addlabel_sjz();
+    void addlabel_ts();
+    void addlabel_zjk();
+
+    void addgansu();//省
+    void addchongqing();
+    void addbeijing();
+    void addhebei();
+
+
+
+signals:
+    void mySignal1();
+
+protected:
+    void paintEvent(QPaintEvent *);
+
+private slots:
+
+
+
+private:
+    Ui::LastWindow *ui;
+    QPushButton b;
+
+
+};
+
+#endif // LASTWINDOW_H
