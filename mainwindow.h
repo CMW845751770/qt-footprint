1eeb86c8349a24a60d77710cf66dda12602b02d9
mantuoluozk <1741695059@qq.com>
2018-12-20 20:40:46 +0800

    Create mainwindow.h

---
+++ mainwindow.h
@@ -0,0 +1,40 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+
+#include <QMainWindow>
+#include <QPushButton>
+#include "subwindow.h"//子窗口头文件
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+    void changeWin();
+    void dealSub();
+    void dealLast();
+
+    //void closeEvent (QCloseEvent *e);
+protected:
+    //重写绘图事件，虚函数
+    //如果在窗口绘图，必须放在绘图事件里实现
+    //绘图事件内部自动调用，窗口需要重绘的时候（状态改变）
+    void paintEvent(QPaintEvent *);
+
+
+private:
+    Ui::MainWindow *ui;
+    QPushButton b1;
+    QPushButton b2;
+
+    SubWindow w;
+};
+
+#endif // MAINWINDOW_H
